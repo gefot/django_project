ea9b6c95f6ee445bfbf3a195f3738c23af569170
gfot <georgios.fotiadis@gmail.com>
2019-02-15 09:06:36 +0200

    New

--- first_project/second_app/urls.py
+++ first_project/second_app/urls.py
@@ -4,7 +4,7 @@ from second_app import views
 # TEMPLATE TAGGING
 app_name = 'second_app'
 
-urlpatterns =[
+urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'devices/', views.second_app_devices, name='second_app_devices'),
     url(r'users/', views.second_app_users, name='second_app_users'),
